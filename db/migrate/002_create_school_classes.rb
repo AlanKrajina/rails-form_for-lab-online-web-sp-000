1e01654f4eca32e2efe61693d6ef38cfaea3c4c6
Alan Krajina <alankrajina88@gmail.com>
2019-08-10 13:04:55 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_school_classes.rb
+++ db/migrate/002_create_school_classes.rb
@@ -1,8 +1,8 @@
 class CreateSchoolClass < ActiveRecord::Migration
   def change
     create_table :school_class do |t|
-      t.string :first_name
-      t.string :last_name
+      t.string :title
+      t.integer :room_number
 
       t.timestamps null: false
     end
