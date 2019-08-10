6360229e35632531c113a4b4aa3ca6df9058cdd2
Alan Krajina <alankrajina88@gmail.com>
2019-08-10 13:15:03 +0000

    Automatically backed up by Learn

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,36 @@
+class StudentsController < ApplicationController
+	def index
+		@students = Student.all
+	end
+
+	def show
+		@student = Student.find(params[:id])
+	end
+
+	def new
+		@student = Student.new
+	end
+
+	def edit
+	  @student = Student.find(params[:id])
+	end
+
+	def create
+	  @student = Student.new(student_params(:title, :description))
+	  @student.save
+	  redirect_to student_path(@student)
+	end
+
+	def update
+	  @student = Student.find(params[:id])
+	  @student.update(student_params(:title))
+	  redirect_to student_path(@student)
+	end
+
+	private
+
+	def student_params(*args)
+	  params.require(:student).permit(*args)
+	end
+
+end
